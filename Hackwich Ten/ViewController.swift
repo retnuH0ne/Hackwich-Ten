7fa1d1c96c8073465f67cf483cf04c4564baf9a8
Hunter Trujillo <megafan.exe@gmail.com>
2026-03-10 11:59:24 -1000

    Completes in-class portion.

--- Hackwich Ten/ViewController.swift
+++ Hackwich Ten/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        myLabel.text = "My Segmented Control"
     }
 
+    
+    @IBAction func segmentedControlPressed(_ sender: Any)
+    {
+        switch segmentedControl.selectedSegmentIndex
+        {
+        case 0: myLabel.text = "First Segment"
+        case 1: myLabel.text = "Second Segment"
+        default: break
+        }
+        
+        
+    }
+    
 
 }
-
